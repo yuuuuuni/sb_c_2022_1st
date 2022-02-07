38ec69e4e8439c94aaea652b3151be50f6d4b013
yuuuuuni <yuuuuuni930302@gmail.com>
2022-02-07 22:27:54 +0900

    호출 시 반환되는 count 값이 1씩 커지는 컨트롤러 액션 구현

--- src/main/java/com/pyh/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/pyh/exam/demo/controller/UsrHomeController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // 일종의 주석이라고 보면 됨. 스프링한테 아래의 클래스가 컨트롤러 라는 것을 알려줌(아래 메서드들을 품고 있으면 써줘야함)
 public class UsrHomeController {
-	@RequestMapping("/usr/home/main") // 이러한 url로 접속이 요청되면
-	@ResponseBody // 아래 함수가 리턴한 값(즉, Body를)을 응답한다(화면에 띄워진다)
+	private int count; // count가 값을 기억하도록 전역변수로 옮김. 여기서는 변수만 선언
+	
+	public UsrHomeController() { // 생성자 생성
+		count = 0; // 여기서 초기값을 0으로 해줌
+	}
+	
+	@RequestMapping("/usr/home/main") // 이렇게 접속이 요청되면
+	@ResponseBody // 아래 함수가 리턴한 값(즉, Body를)을 응답한다
 	public String showMain() {	// showMain()이라는 이 함수가 실행됨. 이 때 showMain() 메서드는 그냥 이름을 지은 것 ddd 이런식으로 지어도 됨
 		return "안녕하세요.";
 	}
@@ -22,6 +28,12 @@ public class UsrHomeController {
 	@ResponseBody
 	public String showMain3() {
 		return "또만나요.";
+	}
+	
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public int showMain4() {
+		return count++; // 후위연산자. count 값을 먼저 리턴하고 뒤늦게 1증가
 	}
 	
 }
