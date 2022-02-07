fde2aa194772be8513e035113256850fffea8faf
yuuuuuni <yuuuuuni930302@gmail.com>
2022-02-07 23:34:34 +0900

    count 조절 액션 구현

--- src/main/java/com/pyh/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/pyh/exam/demo/controller/UsrHomeController.java
@@ -12,35 +12,17 @@ public class UsrHomeController {
 		count = 0; // 여기서 초기값을 0으로 해줌
 	}
 	
-	@RequestMapping("/usr/home/main") // 이렇게 접속이 요청되면
-	@ResponseBody // 아래 함수가 리턴한 값(즉, Body를)을 응답한다
-	public String showMain() {	// showMain()이라는 이 함수가 실행됨. 이 때 showMain() 메서드는 그냥 이름을 지은 것 ddd 이런식으로 지어도 됨
-		return "안녕하세요.";
+	@RequestMapping("/usr/home/getCount")
+	@ResponseBody
+	public int getCount() {
+		return count;
 	}
 	
-	@RequestMapping("/usr/home/main2")
+	@RequestMapping("/usr/home/doSetCount")
 	@ResponseBody
-	public String showMain2() {
-		return "반갑습니다.";
-	}
-	
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public String showMain3() {
-		return "또만나요.";
-	}
-	
-	@RequestMapping("/usr/home/main4")
-	@ResponseBody
-	public int showMain4() {
-		return count++; // 후위연산자. count 값을 먼저 리턴하고 뒤늦게 1증가
-	}
-	
-	@RequestMapping("/usr/home/main5")
-	@ResponseBody
-	public String showMain5() {
-		count = 0;
-		return "count의 값이 0으로 초기화 되었습니다.";
+	public String doSetCount(int count) { // url에서 doSetCount 뒤에 '?변수=값&변수=값&변수=값' 이런식으로 와야함. "?count=50" 이렇게!
+		this.count = count;
+		return "count의 값이 " + this.count + "으로 초기화 되었습니다.";
 	}
 }
 
