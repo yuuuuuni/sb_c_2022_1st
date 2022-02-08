744c1ce56a8600d45a0e75fd31664b574d46c0bb
yuuuuuni <yuuuuuni930302@gmail.com>
2022-02-09 00:15:50 +0900

    테스트용 게시물 10개 자동생성

--- src/main/java/com/pyh/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/pyh/exam/demo/controller/UsrArticleController.java
@@ -12,29 +12,53 @@ import com.pyh.exam.demo.vo.Article;
 @Controller
 public class UsrArticleController {
 	private int articlesLastId;
-	private List<Article> articles; // 먼저, ArrayList<Article>의 변수 articles 선언 
+	private List<Article> articles; // 게시물을 계속 추가해야 하므로 게시물들을 담을 어레이리스트 사용, 이 때 articles라는 변수 먼저 선언
 	
 	public UsrArticleController() {
 		articlesLastId = 0;
-		articles = new ArrayList<>(); // 여기서 ArrayList<> 객체 생성
+		articles = new ArrayList<>(); // 여기서 어레이리스트 객체 생성
+		
+		makeTestData();
 	}
 	
+	private void makeTestData() { // 테스트 게시물 생성
+		for(int i = 1; i <= 10; i++) {
+			int id = articlesLastId + 1;
+			String title = "제목" + i;
+			String body = "내용" + i;
+			Article article = new Article(id, title, body);
+			
+			articles.add(article);
+			articlesLastId = id;
+		}
+		
+	}
+
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
 		int id = articlesLastId + 1;
-		Article article = new Article(id, title, body);
+		Article article = new Article(id, title, body); // 게시물 객체 생성
 		
 		articles.add(article);
 		articlesLastId = id;
-				
-		return article;
+		
+		return article; // 만들어진 새 게시물이 화면에 보여져라
 	}
 	
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		return articles;
+		return articles; // articles라는 어레이리스트를 화면에 보여줘라.(즉, 게시물들을 보여줘라)
+	}
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		
+		return id + "번 게시물을 삭제하였습니다.";
+		
+		
+		
 	}
 }
-
