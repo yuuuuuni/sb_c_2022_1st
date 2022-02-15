698f0e9ce41b5fd2ac8ab552c57ab0994cd7ec57
yuuuuuni <yuuuuuni930302@gmail.com>
2022-02-15 13:51:50 +0900

    SQL을 xml 파일로 옮김

--- src/main/java/com/pyh/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/pyh/exam/demo/repository/ArticleRepository.java
@@ -13,21 +13,15 @@ import com.pyh.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
 	public void writeArticle(@Param("title") String title, @Param("body") String body); // INSERT 쿼리는 값이 삽입되는 것이므로 리턴을 안해줌. 그러므로 타입을 void로 바꾸기
 
-	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(@Param("id") int id);
 
-	@Delete("DELETE FROM article WHERE id = #{id}")
 	public void deleteArticle(@Param("id") int id);
 
-	@Update("UPDATE article SET updateDate = NOW(), title = #{title}, `body` = #{body} where id = #{id}")
 	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
 
-	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 	
-	@Select("SELECT LAST_INSERT_ID()")
 	public int getLastInsertId();
 }
