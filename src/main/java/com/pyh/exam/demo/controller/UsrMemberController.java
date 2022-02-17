15d5d2f607977ae38ea4b708907520dc6f4cee8c
yuuuuuni <yuuuuuni930302@gmail.com>
2022-02-17 22:34:22 +0900

    회원가입을 처리하기 위해, UsrMemberController, MemberService 추가

---
+++ src/main/java/com/pyh/exam/demo/controller/UsrMemberController.java
@@ -0,0 +1,21 @@
+package com.pyh.exam.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.pyh.exam.demo.service.MemberService;
+
+@Controller
+public class UsrMemberController {
+	@Autowired
+	private MemberService memberService;
+
+	@RequestMapping("/usr/member/doJoin")
+	@ResponseBody
+	public String doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		return "성공";
+	}
+}
---
+++ src/main/java/com/pyh/exam/demo/service/MemberService.java
@@ -0,0 +1,16 @@
+package com.pyh.exam.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+	// @Autowired
+	// private MemberRepository memberRepository;
+
+	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		// memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		
+	}
+
+}
