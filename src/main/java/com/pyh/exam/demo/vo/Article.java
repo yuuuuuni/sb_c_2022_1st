a04347202fdefbe7014597d2aa3069dd3029eaf9
yuuuuuni <yuuuuuni930302@gmail.com>
2022-03-24 00:43:49 +0900

    Article 클래스와 Member 클래스에서 골뱅이Data를 달아주는 이유

--- src/main/java/com/pyh/exam/demo/vo/Article.java
+++ src/main/java/com/pyh/exam/demo/vo/Article.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+@Data // 자동으로 GetterSetter를 만들어줌(즉, get(), set() 메소드를 일일이 만들지 않아도 .get~, .set~를 쓸 수 있다는 뜻)
 @NoArgsConstructor // 인자 없는 생성자까지 가능하도록
 @AllArgsConstructor // 이걸 쓰면 생성자 따로 만들어줄 필요 없이 new Article(1, "제목1") 이렇게 써주면 생성자가 자동으로 만들어짐
 public class Article {
