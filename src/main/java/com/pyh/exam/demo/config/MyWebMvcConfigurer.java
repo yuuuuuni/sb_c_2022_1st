3276da5b6995c7d40d029c5c8e70c67eaef286a0
yuuuuuni <yuuuuuni930302@gmail.com>
2022-03-25 14:27:09 +0900

    NeedLogin 인터셉터 구현 및 등록

--- src/main/java/com/pyh/exam/demo/config/MyWebMvcConfigurer.java
+++ src/main/java/com/pyh/exam/demo/config/MyWebMvcConfigurer.java
@@ -1,4 +1,4 @@
-package com.pyh.exam.demo;
+package com.pyh.exam.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -6,8 +6,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.pyh.exam.demo.interceptor.BeforeActionInterceptor;
-
-import lombok.Value;
+import com.pyh.exam.demo.interceptor.NeedLoginInterceptor;
 
 // 이 부분은 프로그램 실행 시 맨 처음 딱 한번만 실행
 @Configuration // 설정 시, 스프링이 맨 처음 실행된 후 자동으로 여기를 가장 먼저 읽음
@@ -16,6 +15,11 @@ import lombok.Value;
 		@Autowired // new하지 않고도 객체 생성
 		BeforeActionInterceptor beforeActionInterceptor; // 이렇게만 선언하면 인터셉터만 있는 것
 
+		// needLoginInterceptor 인터셉터 불러오기
+		@Autowired
+		NeedLoginInterceptor needLoginInterceptor;
+				
+		
 		// 이 함수는 인터셉터를 적용하는 역할을 합니다.
 		@Override
 		public void addInterceptors(InterceptorRegistry registry) {
@@ -24,8 +28,15 @@ import lombok.Value;
 			// 인터셉터한테 아래처럼 지령을 내림
 	        registry.addInterceptor(beforeActionInterceptor) // beforeActionInterceptor라는 인터셉터 추가해라 
 	                .addPathPatterns("/**") // (beforeActionInterceptor한테) 모든 요청에 대해서 인터셉터 실행해라
-	                .excludePathPatterns("/resource/**") // 다만, resource/로 시작하는 경로는 제외해라
+	                .excludePathPatterns("/resource/**") // 다만, resource/로 시작하는 모든 경로는 제외해라
 	                .excludePathPatterns("/error"); // error 보여주는 것도 제외해라
+	        
+	        registry.addInterceptor(needLoginInterceptor)
+            		.addPathPatterns("/usr/article/write") // 글 등록 페이지
+            		.addPathPatterns("/usr/article/doWrite") // 글 등록을 처리해주는 페이지(눈에 안보임)
+            		.addPathPatterns("/usr/article/modify") // 글 수정 페이지
+            		.addPathPatterns("/usr/article/doModify") // 글 수정을 처리해주는 페이지(눈에 안보임)
+            		.addPathPatterns("/usr/article/doDelete"); // 글 삭제를 처리해주는 페이지(눈에 안보임)
 		}
 	}
 
---
+++ src/main/java/com/pyh/exam/demo/interceptor/NeedLoginInterceptor.java
@@ -0,0 +1,20 @@
+package com.pyh.exam.demo.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.pyh.exam.demo.vo.Rq;
+
+@Component // @Autowired 하려면 @Component 붙여줘야함
+public class NeedLoginInterceptor implements HandlerInterceptor {
+	 @Override
+	    public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+		 System.out.println("로그인 필요!!");
+		 
+	        return HandlerInterceptor.super.preHandle(req, resp, handler);
+	    }
+}
