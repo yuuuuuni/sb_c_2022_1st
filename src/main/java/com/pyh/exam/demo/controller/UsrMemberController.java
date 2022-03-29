525282eef713b4bf20d619c0916802da7ad14a15
yuuuuuni <yuuuuuni930302@gmail.com>
2022-03-30 01:49:45 +0900

    로그인 폼

--- src/main/java/com/pyh/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/pyh/exam/demo/controller/UsrMemberController.java
@@ -57,9 +57,14 @@ public class UsrMemberController {
 			return ResultData.newData(joinRd, "member", member); // newData 메소드 이용하여 joinRd의 resultCode, msg는 그대로 가져가되 Data1 부분만 member로 넣어줌)
 	}
 	
+	@RequestMapping("/usr/member/login")
+	public String showLogin(HttpSession httpSession) {
+		return "usr/member/login";
+	}
+	
 	@RequestMapping("/usr/member/doLogin")
 	@ResponseBody
-	public ResultData doLogin(HttpSession httpSession, String loginId, String loginPw) { // HttpSession httpSession을 적어주면 알아서 sessione을 얻어와준다.
+	public String doLogin(HttpSession httpSession, String loginId, String loginPw) { // HttpSession httpSession을 적어주면 알아서 session을 얻어와준다.
 		boolean isLogined = false;
 		
 		if(httpSession.getAttribute("loginedMemberId") != null) { // 이미 로그인 되어있는 상태이면
@@ -67,34 +72,34 @@ public class UsrMemberController {
 		}
 		
 		if(isLogined) {
-			return ResultData.from("F-5", "이미 로그인 된 상태입니다.");
+			return Ut.jsHistoryBack("이미 로그인 된 상태입니다.");
 		}
 		
 		if(Ut.empty(loginId)) {
-			return ResultData.from("F-1", "loginId(을)를 입력해주세요.");
+			return Ut.jsHistoryBack("loginId(을)를 입력해주세요.");
 		}
 		
 		if(Ut.empty(loginPw)) {
-			return ResultData.from("F-2", "loginPw(을)를 입력해주세요.");
+			return Ut.jsHistoryBack("loginPw(을)를 입력해주세요.");
 		}
 		
 		Member member = memberService.getMemberByLoginId(loginId); // member에는 db에 있는 정보가 담기는 것임
 		
 		// 브라우저에서 입력된 loginId에 대한 회원 정보가 없는 경우
 		if(member == null) {
-			return ResultData.from("F-3", "존재하지 않는 로그인아이디 입니다.");
+			return Ut.jsHistoryBack("존재하지 않는 로그인아이디 입니다.");
 		}
 		
 		// 입력된 loginId에 대한 회원 정보는 있지만 그 값(member)의 비밀번호가 브라우저에서 입력된 비밀번호(loginPw)와 같지 않은 경우
 		if(member.getLoginPw().equals(loginPw) == false) {
-			return ResultData.from("F-4", "비밀번호가 일치하지 않습니다.");
+			return Ut.jsHistoryBack("비밀번호가 일치하지 않습니다.");
 		}
 		
 		// 여기서부터는 로그인 성공
 		
 		httpSession.setAttribute("loginedMemberId", member.getId()); // 로그인된 회원의 id를 꺼내서 '로그인된 회원의 id'라고 이름을 지정하겠다.
 		
-		return ResultData.from("S-1", Ut.f("%s님 환영합니다.", member.getNickname()));
+		return Ut.jsReplace(Ut.f("%s님 환영합니다.", member.getNickname()), "/"); // 정상적으로 로그인이 되면 메인 페이지로 가도록 uri를 "/"로 지정
 	}
 	
 	@RequestMapping("/usr/member/doLogout")
