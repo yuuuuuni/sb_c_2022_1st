2119eeaae5ba31948fead9672c25388b4157e58b
yuuuuuni <yuuuuuni930302@gmail.com>
2022-04-08 01:27:40 +0900

    멀티게시판 개념 도입, board 테이블 생성

--- db/schema.sql
+++ db/schema.sql
@@ -88,3 +88,40 @@ SELECT * FROM article;
 UPDATE article
 SET memberId = 2
 WHERE memberId = 0;
+
+# 게시판(board) 테이블 생성
+CREATE TABLE board (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    `code` CHAR(50) NOT NULL UNIQUE COMMENT 'notice(공지사항), free1(자유게시판1), free2(자유게시판2), ...',
+    `name` CHAR(50) NOT NULL  UNIQUE COMMENT '게시판 이름',
+    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '삭제여부 (0=삭제전, 1=삭제)', # 삭제상태 컬럼
+    delDate DATETIME COMMENT '삭제날짜' # 삭제날짜 컬럼, 삭제를 해야만 생기는 컬럼이므로 삭제를 안하면 비어있을 수 있기 때문에 not null로 안해놓음
+);
+
+# 기본 게시판(board) 데이터 생성
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'notice',
+`name` = '공지사항';
+
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'free1',
+`name` = '자유게시판';
+
+# 게시물(article) 테이블에 boardId 컬럼 추가
+ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER memberId;
+
+# 1, 2번 게시물(article)을 공지사항 게시물로 지정
+UPDATE article
+SET boardId = 1
+WHERE id IN (1, 2);
+
+# 3번 게시물(article)을 자유게시판 게시물로 지정
+UPDATE article
+SET boardId = 2
+WHERE id IN (3);
