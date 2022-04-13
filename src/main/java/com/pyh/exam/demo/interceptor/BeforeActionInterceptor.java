0301e1cab55671b6d2f44166924b1c59e30999c3
yuuuuuni <yuuuuuni930302@gmail.com>
2022-04-13 23:30:05 +0900

    BeforeActionInterceptor 에서 Rq 객체를 좀 더 확실하게 생성되도록 빈 메서드를 호출하여 유도

--- src/main/java/com/pyh/exam/demo/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/pyh/exam/demo/interceptor/BeforeActionInterceptor.java
@@ -3,18 +3,22 @@ package com.pyh.exam.demo.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import com.pyh.exam.demo.service.MemberService;
 import com.pyh.exam.demo.vo.Rq;
 
 @Component // @Autowired 하려면 @Component(@Service, @Mapper로도 대체가능) 붙여줘야함
 public class BeforeActionInterceptor implements HandlerInterceptor {
+	private Rq rq;
+	
+	public BeforeActionInterceptor(Rq rq) {
+		this.rq = rq;
+	}
+	
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-		// 이제는 Rq 객체가 자동으로 만들어지기 때문에 필요 없음
+		rq.initOnBeforeActionInterceptor(); // rq를 강제로 활성화시키기 위해 rq에 빈 메소드를 만들어 호출
 
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
