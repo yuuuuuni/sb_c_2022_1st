5da07a7c32d16a059e152db23c0b06485a15706e
yuuuuuni <yuuuuuni930302@gmail.com>
2022-04-21 23:59:19 +0900

    검색창 구현

---
+++ src/main/resources/static/resource/common.js
@@ -0,0 +1,9 @@
+$('select[data-value]').each(function(index, el) {
+	const $el = $(el);
+
+	const defaultValue = $el.attr('data-value').trim();
+
+	if (defaultValue.length > 0) {
+		$el.val(defaultValue);
+	}
+});
